9ac3104be4644885723f16fe25e9cdca2af036de
daedalus31 <marczykowskim@gmail.com>
2017-11-17 08:51:56 +0100

    Added first files of OpenFOAM plugin.

---
+++ pl.unicore.portal.openfoam/src/main/java/pl/edu/icm/unicore/portal/openfoam/ui/OpeFOAMMainView.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2017, ICM University of Warsaw. All rights reserved.
+ * See LICENSE file for licensing information.
+ */
+package pl.edu.icm.unicore.portal.openfoam.ui;
+
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import eu.unicore.portal.core.PortalConfigurationSource;
+import eu.unicore.portal.core.PortalThreadPool;
+import eu.unicore.portal.core.i18n.MessageProvider;
+import eu.unicore.portal.core.userprefs.UserProfilesManager;
+import eu.unicore.portal.ui.views.AbstractView;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Entry point of the TemplatePlugin integrated with UNICORE portal.
+ *
+ * @author R.Kluszczynski
+ */
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class OpeFOAMMainView extends AbstractView {
+    private static final Logger log = Logger.getLogger(OpeFOAMMainView.class);
+
+    public static final String PORTAL_URL_FRAGMENT = "template";
+
+    private final PortalConfigurationSource configurationSource;
+    private final UserProfilesManager profilesManager;
+    private final PortalThreadPool threadPool;
+
+    @Autowired
+    public OpeFOAMMainView(PortalConfigurationSource configurationSource,
+                                  MessageProvider messageProvider,
+                                  PortalThreadPool threadPool,
+                                  UserProfilesManager profilesManager) {
+        super(messageProvider);
+        this.configurationSource = configurationSource;
+        this.threadPool = threadPool;
+        this.profilesManager = profilesManager;
+    }
+
+    @Override
+    protected com.vaadin.ui.Component initializeViewComponent() {
+        setTitle(msgProvider.getMessage("TemplatePlugin.MainView.uiTitle"));
+
+        final VerticalLayout main = new VerticalLayout();
+        main.addComponent(
+                new Label("Template Plugin!", ContentMode.PREFORMATTED)
+        );
+        return main;
+    }
+
+    @Override
+    public String getFragment() {
+        return PORTAL_URL_FRAGMENT;
+    }
+}
---
+++ pl.unicore.portal.openfoam/src/main/java/pl/edu/icm/unicore/portal/openfoam/ui/OpenFOAMNavFactory.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017, ICM University of Warsaw. All rights reserved.
+ * See LICENSE file for licensing information.
+ */
+package pl.edu.icm.unicore.portal.openfoam.ui;
+
+import eu.unicore.portal.ui.menu.AbstractSwitchToMenuEntryFactory;
+import eu.unicore.portal.ui.menu.NavigationMenu;
+import org.springframework.stereotype.Component;
+
+/**
+ * Switch to plugin view in main navigation.
+ *
+ * @author R.Kluszczynski
+ */
+@Component
+public class OpenFOAMNavFactory extends AbstractSwitchToMenuEntryFactory {
+    public OpenFOAMNavFactory() {
+        super("TemplatePlugin.MenuEntry",
+                NavigationMenu.MENU_ID,
+                null,
+                null,
+                OpeFOAMMainView.class.getName());
+    }
+}
