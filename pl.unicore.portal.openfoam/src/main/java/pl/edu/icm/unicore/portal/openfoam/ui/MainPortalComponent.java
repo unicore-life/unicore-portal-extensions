185fb505380ae4642ca6bbf7ea1049bda72a8b8d
daedalus31 <mm@mateuszmarczykowski.pl>
2018-05-04 11:23:55 +0200

    formatting

--- pl.unicore.portal.openfoam/src/main/java/pl/edu/icm/unicore/portal/openfoam/ui/MainPortalComponent.java
+++ pl.unicore.portal.openfoam/src/main/java/pl/edu/icm/unicore/portal/openfoam/ui/MainPortalComponent.java
@@ -21,11 +21,7 @@ import eu.unicore.portal.core.PortalThreadPool;
 import eu.unicore.portal.core.Session;
 import eu.unicore.portal.core.i18n.MessageProvider;
 import eu.unicore.portal.core.userprefs.UserProfilesManager;
-import eu.unicore.portal.grid.ui.actions.AbortJobAction;
-import eu.unicore.portal.grid.ui.actions.BrowseJobDirectoryAction;
-import eu.unicore.portal.grid.ui.actions.DeleteJobAction;
-import eu.unicore.portal.grid.ui.actions.RestartJobAction;
-import eu.unicore.portal.grid.ui.actions.ShowJobDetailsAction;
+import eu.unicore.portal.grid.ui.actions.*;
 import eu.unicore.portal.grid.ui.helpers.BrokeredJobSubmissionHelper;
 import eu.unicore.portal.grid.ui.views.JobsTable;
 import eu.unicore.portal.ui.IconUtil;
@@ -47,191 +43,172 @@ import java.util.Collection;
  *
  * @author K. Benedyczak
  */
-public class MainPortalComponent extends CustomComponent
-{
-	private static final Logger log = Logger.getLogger(MainPortalComponent.class);
+public class MainPortalComponent extends CustomComponent {
+    private static final Logger log = Logger.getLogger(MainPortalComponent.class);
 
-	private OpenFOAMProperties config;
-	private JobHandler jobHandler;
+    private OpenFOAMProperties config;
+    private JobHandler jobHandler;
 
-	private final MessageProvider msg;
+    private final MessageProvider msg;
 
-	private TabSheet simulations;
+    private TabSheet simulations;
 
-	private PortalThreadPool tpool;
+    private PortalThreadPool tpool;
 
-	private UserProfilesManager profilesMan;
+    private UserProfilesManager profilesMan;
 
-	private OpenFOAMGridEnvironment gridEnvironment = new OpenFOAMGridEnvironment();
+    private OpenFOAMGridEnvironment gridEnvironment = new OpenFOAMGridEnvironment();
 
-	public MainPortalComponent(PortalConfigurationSource configSource, MessageProvider msg,
-                               PortalThreadPool tpool, UserProfilesManager profilesMan)
-	{
-		this.msg = msg;
-		this.tpool = tpool;
-		this.profilesMan = profilesMan;
-		config = new OpenFOAMProperties(configSource.getProperties());
-		jobHandler = new JobHandler(new BrokeredJobSubmissionHelper());
-		initUI();
-	}
+    public MainPortalComponent(PortalConfigurationSource configSource, MessageProvider msg,
+                               PortalThreadPool tpool, UserProfilesManager profilesMan) {
+        this.msg = msg;
+        this.tpool = tpool;
+        this.profilesMan = profilesMan;
+        config = new OpenFOAMProperties(configSource.getProperties());
+        jobHandler = new JobHandler(new BrokeredJobSubmissionHelper(), new StorageUtil(config, Session.getCurrent().getUser()));
+        initUI();
+    }
 
-	public void setGridEnvironment(OpenFOAMGridEnvironment gridEnvironment)
-	{
-		this.gridEnvironment = gridEnvironment;
-	}
+    public void setGridEnvironment(OpenFOAMGridEnvironment gridEnvironment) {
+        this.gridEnvironment = gridEnvironment;
+    }
 
     public void setGridEnvironment(BrokerService broker, StorageFactoryService sfs) {
         gridEnvironment.setBrokerService(broker);
         gridEnvironment.setStorageFactoryService(sfs);
-	}
+    }
 
-    private void initUI()
-	{
-		VerticalLayout main = new VerticalLayout();
-		main.setSizeFull();
-		main.setMargin(true);
-		setCompositionRoot(main);
+    private void initUI() {
+        VerticalLayout main = new VerticalLayout();
+        main.setSizeFull();
+        main.setMargin(true);
+        setCompositionRoot(main);
 
         Button submitDateJobButton = new Button("Submit Date Job");
         submitDateJobButton.addClickListener(new DateJobSubmitter(jobHandler, gridEnvironment));
         main.addComponent(submitDateJobButton);
 
-		HorizontalLayout mainToolbar = new HorizontalLayout();
-		main.addComponents(mainToolbar);
+        HorizontalLayout mainToolbar = new HorizontalLayout();
+        main.addComponents(mainToolbar);
 
-		simulations = new TabSheet();
-		simulations.addStyleName(Reindeer.TABSHEET_MINIMAL);
-		simulations.setSizeFull();
+        simulations = new TabSheet();
+        simulations.addStyleName(Reindeer.TABSHEET_MINIMAL);
+        simulations.setSizeFull();
 
-		VerticalLayout wrapperBottom = new VerticalLayout();
-		wrapperBottom.setMargin(new MarginInfo(true, false, false, false));
-		wrapperBottom.addComponent(simulations);
+        VerticalLayout wrapperBottom = new VerticalLayout();
+        wrapperBottom.setMargin(new MarginInfo(true, false, false, false));
+        wrapperBottom.addComponent(simulations);
 
-		final JobApplicationFilter filter = new JobApplicationFilter(
-				OpenFOAMJSDLCreator.APPLICATON_NAME);
-		final SingleContextAction<AtomicJob> openJob = new SingleContextAction<AtomicJob>(
-				msg.getMessage("OpenFOAM.MainComponent.openSimulation"),
-				IconRepository.ICON_ID_VIEW,
-				AtomicJob.class)
-		{
-			@Override
-			public void invoke(AtomicJob job)
-			{
+        final JobApplicationFilter filter = new JobApplicationFilter(
+                OpenFOAMJSDLCreator.APPLICATON_NAME);
+        final SingleContextAction<AtomicJob> openJob = new SingleContextAction<AtomicJob>(
+                msg.getMessage("OpenFOAM.MainComponent.openSimulation"),
+                IconRepository.ICON_ID_VIEW,
+                AtomicJob.class) {
+            @Override
+            public void invoke(AtomicJob job) {
 //				openExistingJob(job);
-			}
-		};
+            }
+        };
 
-		JobsTable table = new JobsTable(msg, tpool, profilesMan)
-		{
-			protected Filter getJobsFilter() {
-				return filter;
-			}
+        JobsTable table = new JobsTable(msg, tpool, profilesMan) {
+            protected Filter getJobsFilter() {
+                return filter;
+            }
 
-			@Override
-			protected void createUI()
-			{
-				super.createUI();
-				table.setPageLength(5);
-			}
+            @Override
+            protected void createUI() {
+                super.createUI();
+                table.setPageLength(5);
+            }
 
-			protected PortalHandler[] getInitHandlers() {
-				UserGridDiscovery userGridDiscovery = Session.getCurrent().getUserGridDiscovery();
-				return new PortalHandler[] {
-						openJob,
-						new BrowseJobDirectoryAction(msgProvider, tpool.getExecutor(), profilesMan),
-						new ShowJobDetailsAction(msgProvider),
-						new DeleteJobAction(msgProvider, tpool.getExecutor(), userGridDiscovery),
-						new AbortJobAction(msgProvider, tpool.getExecutor(), userGridDiscovery),
-						new RestartJobAction(msgProvider, tpool.getExecutor(), userGridDiscovery)
-					};
-			}
+            protected PortalHandler[] getInitHandlers() {
+                UserGridDiscovery userGridDiscovery = Session.getCurrent().getUserGridDiscovery();
+                return new PortalHandler[]{
+                        openJob,
+                        new BrowseJobDirectoryAction(msgProvider, tpool.getExecutor(), profilesMan),
+                        new ShowJobDetailsAction(msgProvider),
+                        new DeleteJobAction(msgProvider, tpool.getExecutor(), userGridDiscovery),
+                        new AbortJobAction(msgProvider, tpool.getExecutor(), userGridDiscovery),
+                        new RestartJobAction(msgProvider, tpool.getExecutor(), userGridDiscovery)
+                };
+            }
 
-			protected Collection<String> getColumnsCollapsedByDefault()
-			{
-				return Sets.newHashSet(ShowJobDetailsAction.DETAIL_ESTIMATED_FINISH,
-						ShowJobDetailsAction.DETAIL_EXIT_CODE,
-						ShowJobDetailsAction.DETAIL_APPLICATION,
-						ShowJobDetailsAction.DETAIL_TAGS);
-			}
-		};
+            protected Collection<String> getColumnsCollapsedByDefault() {
+                return Sets.newHashSet(ShowJobDetailsAction.DETAIL_ESTIMATED_FINISH,
+                        ShowJobDetailsAction.DETAIL_EXIT_CODE,
+                        ShowJobDetailsAction.DETAIL_APPLICATION,
+                        ShowJobDetailsAction.DETAIL_TAGS);
+            }
+        };
 
 
-		Button newSimulationButton = new Button("Upload file");
+        Button newSimulationButton = new Button("Upload file");
 //		Button newSimulationButton = new Button(
 //				msg.getMessage("OpenFOAM.MainComponent.newSimulationButton"));
-		newSimulationButton.setIcon(IconUtil.getIconFromTheme(IconRepository.ICON_ID_NEW_FILE));
-		newSimulationButton.addClickListener(new Button.ClickListener()
-		{
-			@Override
-			public void buttonClick(Button.ClickEvent event)
-			{
-				OpenFOAMJobPanel jobPanel = new OpenFOAMJobPanel(config, jobHandler, msg, tpool,
-						Session.getCurrent().getUserGridDiscovery(), gridEnvironment);
-				addTab(jobPanel);
-			}
-		});
+        newSimulationButton.setIcon(IconUtil.getIconFromTheme(IconRepository.ICON_ID_NEW_FILE));
+        newSimulationButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                OpenFOAMJobPanel jobPanel = new OpenFOAMJobPanel(config, jobHandler, msg, tpool,
+                        Session.getCurrent().getUserGridDiscovery(), gridEnvironment);
+                addTab(jobPanel);
+            }
+        });
 
-		final VerticalLayout vl = new VerticalLayout();
-		vl.addComponents(table);
+        final VerticalLayout vl = new VerticalLayout();
+        vl.addComponents(table);
 
-		final Button hideJobsTable = new Button(
-				msg.getMessage("OpenFOAM.MainComponent.hideJobsTableButton"));
-		hideJobsTable.setData(true);
-		hideJobsTable.setIcon(IconUtil.getIconFromTheme(IconRepository.ICON_ID_UPLOAD_FILE));
-		hideJobsTable.addClickListener(new Button.ClickListener()
-		{
-			@Override
-			public void buttonClick(Button.ClickEvent event)
-			{
-				updateShowHideButton(hideJobsTable, vl);
-			}
-		});
+        final Button hideJobsTable = new Button(
+                msg.getMessage("OpenFOAM.MainComponent.hideJobsTableButton"));
+        hideJobsTable.setData(true);
+        hideJobsTable.setIcon(IconUtil.getIconFromTheme(IconRepository.ICON_ID_UPLOAD_FILE));
+        hideJobsTable.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                updateShowHideButton(hideJobsTable, vl);
+            }
+        });
 
-		mainToolbar.addComponents(newSimulationButton, hideJobsTable);
-		mainToolbar.addStyleName(Styles.MARGIN_TOP_BOTTOM_15);
-		mainToolbar.setSpacing(true);
+        mainToolbar.addComponents(newSimulationButton, hideJobsTable);
+        mainToolbar.addStyleName(Styles.MARGIN_TOP_BOTTOM_15);
+        mainToolbar.setSpacing(true);
 
-		main.addComponents(vl, wrapperBottom);
-	}
+        main.addComponents(vl, wrapperBottom);
+    }
 
-	private void updateShowHideButton(Button hideJobsTable, Component toHide)
-	{
-		if (hideJobsTable.getData().equals(true))
-		{
-			hideJobsTable.setCaption(msg.getMessage("OpenFOAM.MainComponent.showJobsTableButton"));
-			hideJobsTable.setData(false);
-			toHide.setVisible(false);
-		} else
-		{
-			hideJobsTable.setCaption(msg.getMessage("OpenFOAM.MainComponent.hideJobsTableButton"));
-			hideJobsTable.setData(true);
-			toHide.setVisible(true);
-		}
-	}
+    private void updateShowHideButton(Button hideJobsTable, Component toHide) {
+        if (hideJobsTable.getData().equals(true)) {
+            hideJobsTable.setCaption(msg.getMessage("OpenFOAM.MainComponent.showJobsTableButton"));
+            hideJobsTable.setData(false);
+            toHide.setVisible(false);
+        } else {
+            hideJobsTable.setCaption(msg.getMessage("OpenFOAM.MainComponent.hideJobsTableButton"));
+            hideJobsTable.setData(true);
+            toHide.setVisible(true);
+        }
+    }
 
-	private void openExistingJob(AtomicJob job)
-	{
-		OpenFOAMJobSpecification jobSpec;
-		try
-		{
-			jobSpec = OpenFOAMJSDLCreator.createJobSpecification(job.getProperties());
-		} catch (Exception e)
-		{
-			log.error("Error parsing job's properties", e);
-			Notification.show(msg.getMessage("OpenFOAM.MainComponent.errorParseJob", e.toString()),
-					Type.ERROR_MESSAGE);
-			return;
-		}
-		OpenFOAMJobPanel jobPanel = new OpenFOAMJobPanel(config, jobSpec, job, jobHandler, msg,
-				tpool, Session.getCurrent().getUserGridDiscovery(), gridEnvironment);
-		addTab(jobPanel);
-	}
-//
-	private void addTab(OpenFOAMJobPanel jobPanel)
-	{
-		Tab tab = simulations.addTab(jobPanel);
-		tab.setClosable(true);
-		jobPanel.setParent(tab);
-		simulations.setSelectedTab(tab);
-	}
+    private void openExistingJob(AtomicJob job) {
+        OpenFOAMJobSpecification jobSpec;
+        try {
+            jobSpec = OpenFOAMJSDLCreator.createJobSpecification(job.getProperties());
+        } catch (Exception e) {
+            log.error("Error parsing job's properties", e);
+            Notification.show(msg.getMessage("OpenFOAM.MainComponent.errorParseJob", e.toString()),
+                    Type.ERROR_MESSAGE);
+            return;
+        }
+        OpenFOAMJobPanel jobPanel = new OpenFOAMJobPanel(config, jobSpec, job, jobHandler, msg,
+                tpool, Session.getCurrent().getUserGridDiscovery(), gridEnvironment);
+        addTab(jobPanel);
+    }
+
+    //
+    private void addTab(OpenFOAMJobPanel jobPanel) {
+        Tab tab = simulations.addTab(jobPanel);
+        tab.setClosable(true);
+        jobPanel.setParent(tab);
+        simulations.setSelectedTab(tab);
+    }
 }
